223fb4c19109d4bce95834ddd652f672c9c81d38
KKTRKKT <paulhana6006@gmail.com>
2022-04-23 14:06:49 +0900

    로컬변수 이동

--- src/main/java/function/NumberPrinter.java
+++ src/main/java/function/NumberPrinter.java
@@ -1,27 +1,23 @@
 package function;
 
 public class NumberPrinter {
-    int column;
-    int pagenumber;
-    int pageoffset;
-    int rowoffset;
     int linesPerPage;
     int columns;
 
     public NumberPrinter(int linesPerPage, int columns) {
         this.linesPerPage = linesPerPage;
         this.columns = columns;
-        pagenumber = 1;
-        pageoffset = 1;
     }
 
     public void printNumbers(int numberOfPrimes, int[] primes) {
+        int pagenumber = 1;
+        int pageoffset = 1;
         while (pageoffset <= numberOfPrimes) {
             System.out.println("The First " + numberOfPrimes +
                     " Prime Numbers --- Page " + pagenumber);
             System.out.println("");
-            for (rowoffset = pageoffset; rowoffset < pageoffset + linesPerPage; rowoffset++) {
-                for (column = 0; column < columns; column++)
+            for (int rowoffset = pageoffset; rowoffset < pageoffset + linesPerPage; rowoffset++) {
+                for (int column = 0; column < columns; column++)
                     if (rowoffset + column * linesPerPage <= numberOfPrimes)
                         System.out.format("%10d", primes[rowoffset + column * linesPerPage]);
                 System.out.println("");
