b8c2d98bb1c1e4e92360629f85610cef2c951117
KKTRKKT <paulhana6006@gmail.com>
2022-04-23 14:01:49 +0900

    기존 숫자 상수로 뺌

--- src/main/java/function/PrintPrimes.java
+++ src/main/java/function/PrintPrimes.java
@@ -1,12 +1,15 @@
 package function;
 
 public class PrintPrimes {
+    static final int numberOfPrimes = 1000;
+    static final int linesPerPage = 50;
+    static final int columns = 4;
 
     public static void main(String[] args) {
-        final int numberOfPrimes = 1000;
         PrimeGenerator generator = new PrimeGenerator();
         int[] primes = generator.generatePrimes(numberOfPrimes);
-        new NumberPrinter(50, 4).printNumbers(numberOfPrimes, primes);
+        NumberPrinter printer = new NumberPrinter(linesPerPage, columns);
+        printer.printNumbers(numberOfPrimes, primes);
     }
 
 }
